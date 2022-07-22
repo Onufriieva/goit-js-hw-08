ce3d525c3bf4206c735c025dc634b7919676662a
Onufriieva <onufrieva.julia@gmail.com>
2022-07-23 01:47:24 +0300

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,29 +7,29 @@ const player = new Player(iframe);
 
 const storage = localStorage.getItem('videoplayer-current-time');
 
-    player.on('timeupdate',
-     _.throttle(function() {
+    // player.on('timeupdate',
+    //  _.throttle(function() {
         
-        player.setCurrentTime(storage).then(function(seconds) {
-            localStorage.setItem('videoplayer-current-time', seconds);
-            // seconds = the actual time that the player seeked to
-        }).catch(function(error) {
-            switch (error.name) {
-                // case 'RangeError':
-                    // the time was less than 0 or greater than the video’s duration
-                    // break;
+    //     player.setCurrentTime(storage).then(function(seconds) {
+    //         localStorage.setItem('videoplayer-current-time', seconds);
+    //         // seconds = the actual time that the player seeked to
+    //     }).catch(function(error) {
+    //         switch (error.name) {
+    //             case 'RangeError':
+    //                 // the time was less than 0 or greater than the video’s duration
+    //                 break;
         
-                // default:
-                    // some other error occurred
-                    // break;
-            }
-        });
-    }, 1000),
-    player
-          .setCurrentTime(storage)
-          .then(function (seconds) {})
-          .catch(function (error) {})
-    );  
+    //             default:
+    //                 // some other error occurred
+    //                 break;
+    //         }
+    //     });
+    // }, 1000),
+    // player
+    //       .setCurrentTime(storage)
+    //       .then(function (seconds) {})
+    //       .catch(function (error) {})
+    // );  
 
 
 
@@ -38,16 +38,16 @@ const storage = localStorage.getItem('videoplayer-current-time');
     
     // const savedTime = localStorage.getItem('videoplayer-current-time');
 
-    // player.on(
-    //     'timeupdate', throttle(function () {
-    //       player.getCurrentTime()
-    //         .then(function (seconds) {
-    //           localStorage.setItem('videoplayer-current-time', seconds);
-    //         })
-    //         .catch(function (error) {});
-    //     }, 1000),
-    //     player
-    //       .setCurrentTime(savedTime)
-    //       .then(function (seconds) {})
-    //       .catch(function (error) {})
-    //   );
+    player.on(
+        'timeupdate', throttle(function () {
+          player.getCurrentTime()
+            .then(function (seconds) {
+              localStorage.setItem('videoplayer-current-time', seconds);
+            })
+            .catch(function (error) {});
+        }, 1000),
+        player
+          .setCurrentTime(storage)
+          .then(function (seconds) {})
+          .catch(function (error) {})
+      );
