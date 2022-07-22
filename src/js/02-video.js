7b55566497cc5ba37de75d48d0ed1261212c2ab8
Onufriieva <onufrieva.julia@gmail.com>
2022-07-23 01:57:24 +0300

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,37 +7,6 @@ const player = new Player(iframe);
 
 const storage = localStorage.getItem('videoplayer-current-time');
 
-    // player.on('timeupdate',
-    //  _.throttle(function() {
-        
-    //     player.setCurrentTime(storage).then(function(seconds) {
-    //         localStorage.setItem('videoplayer-current-time', seconds);
-    //         // seconds = the actual time that the player seeked to
-    //     }).catch(function(error) {
-    //         switch (error.name) {
-    //             case 'RangeError':
-    //                 // the time was less than 0 or greater than the video’s duration
-    //                 break;
-        
-    //             default:
-    //                 // some other error occurred
-    //                 break;
-    //         }
-    //     });
-    // }, 1000),
-    // player
-    //       .setCurrentTime(storage)
-    //       .then(function (seconds) {})
-    //       .catch(function (error) {})
-    // );  
-
-
-
-    // const iframe = document.querySelector('#vimeo-player');
-    // const player = new Vimeo(iframe);
-    
-    // const savedTime = localStorage.getItem('videoplayer-current-time');
-
     player.on(
         'timeupdate', throttle(function () {
           player.getCurrentTime()
