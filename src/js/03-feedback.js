7042f22f6d7b3c6ecba571e37f9493941101afc3
Onufriieva <onufrieva.julia@gmail.com>
2022-07-25 13:59:41 +0300

    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,38 +1,42 @@
-// Получить ссылки на инпут и на текстэрия
-// Повесить на них слушателя
-// Написать функцию, которая будет проверять пустая форма или нет и сохранять в хранилище, если непустая
 
 const formRem = document.querySelector('.feedback-form')
-// const inputRem = document.querySelector('input');
-// const textRem = document.querySelector('textarea');
+const inputRem = document.querySelector('input');
+const textRem = document.querySelector('textarea');
 
-// inputRem.addEventListener('input', onInputChange);
-// textRem.addEventListener('input', onInputChange);
+inputRem.addEventListener('input', onInputChange);
+textRem.addEventListener('input', onInputChange);
 
 const storageKey = "feedback-form-state";
 
 updateInput();
 
 
-formRem.addEventListener('submit', (onFormChange) => {
+formRem.addEventListener('submit', (e) => {
     e.preventDefault();
-    localStorage.setItem(storageKey, formRem.elements.value)
+   
     updateInput();
+    onInputChange();
+    const inputListSaved = localStorage.getItem(storageKey);
+    const inputListParsed = JSON.parse(inputListSaved);
+    console.log(inputListParsed); 
+
     formRem.reset();
  
-})
-
+    localStorage.removeItem(storageKey);   
+});
 
 
 function updateInput() {
 formRem.elements.message.value = localStorage.getItem(storageKey) || '';
-}
+};
 
+function onInputChange () {
+    
+    const inputList = {
+        inputRem: inputRem.value,
+        textRem: textRem.value,
+    };
 
-function onFormChange (e) {
-     const {
-        elements: { email, message },
-     } = e.currentTarget;
-    console.log(email.value, message.value);
-}
+ localStorage.setItem(storageKey, JSON.stringify(inputList))
+};
 
