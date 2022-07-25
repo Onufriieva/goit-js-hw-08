7926c1c99709df31f55683ddffc4f5899e5adc4a
Onufriieva <98782111+Onufriieva@users.noreply.github.com>
2022-07-25 11:00:11 +0000

    Deploying to gh-pages from @ Onufriieva/goit-js-hw-08@7042f22f6d7b3c6ecba571e37f9493941101afc3 🚀

--- 03-feedback.a955ecd4.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector(".feedback-form");function a(){t.elements.message.value=localStorage.getItem("feedback-form-state")||""}a(),t.addEventListener("submit",(o=>{e.preventDefault(),localStorage.setItem("feedback-form-state",t.elements.value),a(),t.reset()}));
-//# sourceMappingURL=03-feedback.a955ecd4.js.map
